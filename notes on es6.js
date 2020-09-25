73e1eedfbb303fb371b5cc4fff7800e9ac0c052a
katratzi <clifford@Richards-MacBook-Pro.local>
2020-09-25 20:01:57 +0800

    object literal

--- notes on es6.js
+++ notes on es6.js
@@ -17,3 +17,13 @@ const {
 } = player;
 
 console.log(`${name} lives in ${city}`);
+
+// object literal
+// helps reduce amount of code we have to write
+function addressMaker(city, state) {
+  const newAdress = { city, state }; // just pass straight in
+
+  console.log(newAdress);
+}
+
+addressMaker("Austin", "Texas");
