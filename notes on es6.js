6745a0a74f096b40d1981cde1475fb0d8a6f9986
katratzi <clifford@Richards-MacBook-Pro.local>
2020-09-25 20:43:29 +0800

    arrow functions - create less code

--- notes on es6.js
+++ notes on es6.js
@@ -65,3 +65,28 @@ function add(...nums) {
 }
 
 add(4, 5, 7, 8, 12);
+
+// arrow functions
+//function declaration
+function breakfastMenu() {
+  return "I'm going to scrambled eggs for breakfast";
+}
+
+//anonymous function
+const lunchMenu = function () {
+  return "I'm going to eat pizza for lunch";
+};
+
+// a basic arrow function (it's a nice shorthand)
+const dinnerMenu = () => {
+  return "I'm going to eat a burger for dinner";
+};
+
+// same but with a value
+const dinnerMenu = (food) => {
+  return `I'm going to eat a ${food} for dinner`;
+};
+console.log(dinnerMenu("chicken salad"));
+
+// if we're just returning one value can omit the return and  brackets for neatness
+const dinnerMenu = (food) => `I'm going to eat a ${food} for dinner`;
