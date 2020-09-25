ac382c7b07283bd50a2b6651dc8ad95b0c4fc82d
katratzi <clifford@Richards-MacBook-Pro.local>
2020-09-25 20:29:50 +0800

    spread operator

--- notes on es6.js
+++ notes on es6.js
@@ -36,3 +36,24 @@ for (const income of incomes) {
   console.log(income);
   total += income;
 }
+
+// spread operator
+// easy way to copy contents of an array into another
+const contacts = ["Mary", "Joel", "Danny"];
+let personalFriends = ["David", ...contacts, "Lily"];
+contacts.push("John");
+
+console.log(personalFriends);
+
+// can also use it with objects too
+let person = {
+  name: "Adam",
+  age: 25,
+  city: "Manchester",
+};
+
+let employee = {
+  ...person, // spread here
+  salary: 50000,
+  position: "Software Developer",
+};
