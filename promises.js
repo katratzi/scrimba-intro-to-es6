fc9a5fdccaac0b1b4547c5f5a0f9e5df02ab1b1c
katratzi <clifford@Richards-MacBook-Pro.local>
2020-09-26 12:25:39 +0800

    fetch call

--- promises.js
+++ promises.js
@@ -25,3 +25,23 @@ const buyFlightTicket = () => {
 buyFlightTicket()
   .then((success) => console.log(success)) // if everything has gone ok we call - THEN
   .catch((error) => console.log(error)); // otherwide we CATCHß the error
+
+// fetch
+// speeds up dealing with api calls
+
+fetch("https://jsonplaceholder.typicode.com/comments/1")
+  .then((response) => response.json()) // initial response...we just want the json so we call another .then
+  .then((data) => console.log(data)); // now we hav the data we just log it out
+
+// here we POST data instead
+fetch("https://jsonplaceholder.typicode.com/comments", {
+  method: "POST",
+  body: JSON.stringify({
+    postId: 1,
+    name: "Bob",
+    email: "bob@bobson.com",
+    body: "Hello World",
+  }),
+})
+  .then((response) => response.json())
+  .then((data) => console.log(data));
